b96ce6e6eb31168d2ff64311154c265c31a2a64c
李东焯 <lidongzhuo@ticpsh.com>
2024-07-08 22:58:45 +0800

    [feat] add stack usage -- compute expression‘s value

---
+++ include/Stack/StackUsage.h
@@ -0,0 +1,12 @@
+#ifndef __STACK_USAGE_H__
+#define __STACK_USAGE_H__
+
+#include <string>
+
+std::string convertToSuffixExpr(std::string expr);
+
+int computeSuffixExpr(std::string suffix_expr);
+
+int computeExpr(std::string expr);
+
+#endif
